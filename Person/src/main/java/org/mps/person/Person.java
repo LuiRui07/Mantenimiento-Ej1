d0618ee551df9332bca20f63ce25d00a3f881a9e
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-02-14 19:42:32 +0100

    Creacion de Pruebas y Mejora Funcinamiento Person.java

--- Person/src/main/java/org/mps/person/Person.java
+++ Person/src/main/java/org/mps/person/Person.java
@@ -19,22 +19,38 @@ public class Person {
     /**
      * Constructs a person with a name, age and gender.
      *
-     * @param name the name of the person
-     * @param age the age of the person
+     * @param name   the name of the person
+     * @param age    the age of the person
      * @param gender the gender of the person
      */
 
     public Person(String name, int age, String gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+        if (name == "") {
+            throw new RuntimeException("Empty Name");
+        } else {
+            this.name = name;
+        }
+        if (age < 0) {
+            throw new RuntimeException("Wrong Age");
+        } else {
+            this.age = age;
+        }
+        if (gender != "Male" && gender != "Female") {
+            throw new RuntimeException("Wrong Gender");
+        } else {
+            this.gender = gender;
+        }
     }
 
-    public String name(){ return name;}
+    public String name() {
+        return name;
+    }
 
-    public int age(){ return age;}
+    public int age() {
+        return age;
+    }
 
-    public String gender(){
+    public String gender() {
         return gender;
     }
 
@@ -42,27 +58,35 @@ public class Person {
      * Computes the average age of male and female persons in a list and returns the result in
      * an array of two elements (the first element is the male mean age and the second one is the
      * female mean age)
+     *
      * @param persons
      * @return
+     */
+
+    /**
+     *
+     * Sumamos el total de las edades de cada general, a la par que sumamos la cantidad de personas de cada genero.
+     * Finalmente dividimos esta suma de edades entre el total de personas de cada genero.
      */
     public double[] averageAgePerGender(List<Person> persons) {
         int males = 0;
         int females = 0;
-        int malesage = 0;
-        int femalesage = 0;
+        int malesAge = 0;
+        int femalesAge = 0;
         for (Person p : persons) {
             if (p.gender().equals("Male")) {
-                malesage += p.age;
+                malesAge += p.age;
                 males++;
-            } else if (p.gender().equals("Female")) {
-                femalesage += p.age;
-                females++;
             } else {
-                throw new Error("Wrong Gender");
+                femalesAge += p.age;
+                females++;
             }
         }
-        double[] res = {malesage/males, femalesage/females};
-        return res;
-    }
 
+            double averageMales = males == 0 ? 0 : (double) malesAge / males;
+            double averageFemales = females == 0 ? 0 : (double) femalesAge / females;
+            double[] res = {averageMales, averageFemales};
+            return res;
+
+    }
 }
--- Person/src/test/java/PersonTest.java
+++ Person/src/test/java/PersonTest.java
@@ -1,2 +1,141 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mps.person.Person;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class PersonTest {
-}
+
+    Person p ;
+
+    @BeforeEach
+    /*
+    Inicializamos la variable p (Persona) antes de cada test.
+    */
+    void setUp(){
+        p = new Person("Pepe",20,"Male");
+    }
+
+    /*
+    Tras cada test establecemos como nula la variable p (Persona) con la que probamos, para evitar problemas.
+    */
+    @AfterEach
+    void shutDown(){
+        Person p = null;
+    }
+
+    /*
+    Comprobamos que el metodo name() devuelve el nombre correctamente.
+    */
+    @Test
+    void NameGetter(){
+        assertEquals("Pepe",p.name());
+    }
+
+    /*
+    Comprobamos que el metodo age() devuelve la edad correctamente.
+    */
+    @Test
+    void AgeGetter(){
+        assertEquals(20,p.age());
+    }
+
+    /*
+    Comprobamos que el metodo gender() devuelve el genero correctamente.
+    */
+    @Test
+    void GenderGetter(){
+        assertEquals("Male",p.gender());
+    }
+
+    /*
+    Comprobamos que lance una excepcion ante una edad erronea.
+    */
+    @Test
+    void WrongAge(){
+        Exception thrown = assertThrows(
+                RuntimeException.class,
+                () -> p = new Person("Pepe",-1,"Male"));
+        assertTrue(thrown.getMessage().contentEquals("Wrong Age"));
+    }
+
+    /*
+    Comprobamos que lance una excepcion ante un genero erroneo.
+    */
+    @Test
+    void WrongGender(){
+        Exception thrown = assertThrows(
+                RuntimeException.class,
+                () -> p = new Person("Pepe",10,"Lala"));
+        assertTrue(thrown.getMessage().contentEquals("Wrong Gender"));
+    }
+
+    /*
+    Comprobamos que lance una excepcion ante una nombre nulo.
+    */
+    @Test
+    void EmptyName(){
+        Exception thrown = assertThrows(
+                RuntimeException.class,
+                () -> p = new Person("",10,"Lala"));
+        assertTrue(thrown.getMessage().contentEquals("Empty Name"));
+    }
+
+    /*
+    Comprobamos que funcione el metodo averageAgePerGender.
+    */
+    @Test
+    void CorrectAverage(){
+        Person p1 = new Person("Marta", 25, "Female");
+        Person p2 = new Person("Alba", 28, "Female");
+        Person p3 = new Person("Sonia", 19, "Female");
+
+        Person p4 = new Person("Jaime", 21, "Male");
+        Person p5 = new Person("Joel", 45, "Male");
+        Person p6 = new Person("Carles", 14, "Male");
+
+        List<Person> persons = Arrays.asList(p1, p2, p3, p4, p5, p6);
+
+        double [] averages = p.averageAgePerGender(persons);
+        assertEquals(24.0, averages[1], 0.1);
+        assertEquals(26.66, averages[0], 0.1);
+    }
+
+    /*
+    Comprobamos que funcione el metodo averageAgePerGender ante un numero nulo de varones.
+    */
+    @Test
+    void EmptyMales(){
+        Person p1 = new Person("Marta", 25, "Female");
+        Person p2 = new Person("Alba", 28, "Female");
+        Person p3 = new Person("Sonia", 19, "Female");
+
+        List<Person> persons = Arrays.asList(p1, p2, p3);
+
+        double [] averages = p.averageAgePerGender(persons);
+        assertEquals(24.0, averages[1], 0.1);
+        assertEquals(0.00, averages[0], 0.1);
+    }
+
+    /*
+    Comprobamos que funcione el metodo averageAgePerGender ante un numero nulo de mujeres.
+    */
+    @Test
+    void EmptyFemales(){
+        Person p4 = new Person("Jaime", 21, "Male");
+        Person p5 = new Person("Joel", 45, "Male");
+        Person p6 = new Person("Carles", 14, "Male");
+
+        List<Person> persons = Arrays.asList(p4,p5,p6);
+
+        double [] averages = p.averageAgePerGender(persons);
+        assertEquals(00.0, averages[1], 0.1);
+        assertEquals(26.66, averages[0], 0.1);
+    }
+
+    }
